6e5923f6d0a5e12ff10c96f49037085d3f4b332d
823042332@qq.com <823042332@qq.com>
2025-04-23 19:41:55 +0800

    update time

--- toolsz/time.py
+++ toolsz/time.py
@@ -1,13 +1,17 @@
 """ 时间小工具 """
 from datetime import datetime
 
-def get_today():
+def get_today(type=''):
     """返回当前本地日期，格式为 'YYYY-MM-DD' 的字符串。
 
     Returns:
         str: 当前本地日期，格式为 'YYYY-MM-DD'
     """
     local_time = datetime.today()
-    local_time = local_time.strftime("%Y-%m-%d")
-    # local_time = local_time.strftime("%Y-%m-%d %H:%M:%S")
+    if type == 'date':
+        local_time = local_time.strftime("%Y-%m-%d")
+    elif type == 'time':
+        local_time = local_time.strftime("%H:%M:%S")
+    else:
+        local_time = local_time.strftime("%Y-%m-%d %H:%M:%S")
     return local_time
